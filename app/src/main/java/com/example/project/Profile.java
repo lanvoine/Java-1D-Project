997181e366294ddad870c03e1e1fd3d9b0a53e96
wes903 <67193619+wes903@users.noreply.github.com>
2020-12-02 02:03:58 +0800

    updated Drawables folder

---
+++ app/src/main/java/com/example/project/Profile.java
@@ -0,0 +1,31 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class Profile extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+
+        View view;
+        ToggleButton myPost_Saved = (ToggleButton) view.findViewById(myPost_Saved);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                } else {
+                    // The toggle is disabled
+                }
+            }
+        });
+
+    }
+}
