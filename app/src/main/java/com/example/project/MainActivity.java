701e0662a4aa48fec8dad86a13f654ea4cead5ec
jjbecomespheh <jjbecomespheh@users.noreply.github.com>
2020-12-02 02:02:26 +0800

    Home page (Top & Bottom bar, drawables)

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.project.Utils.BottomNavViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting...");
+        setupBottomNavView();
+    }
+
+    //Bottom Nav View Setup
+    private void setupBottomNavView(){
+        Log.d(TAG, "setupBottomNavView: Setting up Bottom Navigation View...");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById((R.id.home_bottom_toolbar));
+        BottomNavViewHelper.setupBottomNavView(bottomNavigationViewEx);
+
     }
 }
